8832a51863e1ebe0a5169f698c74d4a33845b120
luis-alencar <luisfernando.fa@hotmail.com>
2021-09-05 19:11:20 -0300

    criação do projeto

---
+++ backend/src/main/java/br/pids/records/SchoolRecordsApplication.java
@@ -0,0 +1,13 @@
+package br.pids.records;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolRecordsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolRecordsApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/br/pids/records/SchoolRecordsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.pids.records;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolRecordsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
