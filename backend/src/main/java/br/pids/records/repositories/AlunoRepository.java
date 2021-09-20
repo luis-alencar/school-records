e794557228afd9e41a581e9784a043e7366cb12b
Gelzieny <gelzieny@gmail.com>
2021-09-20 20:07:23 -0300

    Conectando ao repositorio

--- backend/src/main/java/br/pids/records/repositories/AlunoRepository.java
+++
@@ -1,9 +0,0 @@
-package br.pids.records.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import br.pids.records.model.Aluno;
-
-public interface AlunoRepository extends JpaRepository<Aluno, Long>{
-
-}
--- frontend/src/app/empresa/empresa.ts
+++
@@ -1,5 +0,0 @@
-export class Empresa{
-  id!: number;
-  nome!: string;
-  projeto!: string;
-}
